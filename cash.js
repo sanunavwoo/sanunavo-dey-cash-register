2c01f18f859ccacb5b3f139e6dfb05014f1222c1
sanunavwoo <89309027+sanunavwoo@users.noreply.github.com>
2022-09-01 16:30:51 +0530

    Update cash.js
    fix- negative, number, and null value field validation

--- cash.js
+++ cash.js
@@ -12,12 +12,7 @@ nextbtn.addEventListener("click", function validateBillAmmount(){
     errorMessage.style.display="none";
     
     var billAmmountValue= billAmmount.value;
-    if(billAmmountValue=="")
-    {
-        alert("Please enter a bill amount");
-    }
-    else{
-        if(!(isNaN(billAmmountValue))){
+    if(!(isNaN(billAmmountValue))){
         if(billAmmountValue>0){
             //Unhide the rest of the section
             remainingDiv.style.display="flex";
@@ -28,13 +23,10 @@ nextbtn.addEventListener("click", function validateBillAmmount(){
             showErrorMessage("Bill should be greater than 0. Everything has a price- John Wick's villain");
         }
     }
-        else{
-            errorMessage.style.display= "block";
-            showErrorMessage("Bill Value should be a number")
-        }    
-        
-    }
-    
+    else{
+        errorMessage.style.display= "block";
+        showErrorMessage("Bill Value should be a number")
+    }    
         
 });
 
@@ -42,16 +34,34 @@ checkbtn.addEventListener("click", ()=>{
     errorMessage.style.display="none";
     var billAmmountValue= billAmmount.value;
     var cashGivenValue= cashGiven.value;
-    var ammountToBeReturned= cashGivenValue-billAmmountValue;
-    console.log(ammountToBeReturned);
-    if(ammountToBeReturned>0){
-        calculateChange(ammountToBeReturned);
+    if(cashGivenValue<0){
+        alert("Cash value can't be negative");
+    }
+    else if(billAmmountValue=="" || cashGivenValue==""){
+        alert("Please fill up all fields");
     }
     else{
-        //Bill is more than cash value. Invalid
-        errorMessage.style.display= "block";
-        showErrorMessage("Bill is more than the cash you have given. No Bargaining here!");
+        var ammountToBeReturned= cashGivenValue-billAmmountValue;
+        console.log(ammountToBeReturned);
+        if(ammountToBeReturned>0){
+            calculateChange(ammountToBeReturned);
+        }
+        else{
+            //Bill is more than cash value. Invalid
+            errorMessage.style.display= "block";
+            showErrorMessage("Bill is more than the cash you have given. No Bargaining here!");
+        }
     }
+    // var ammountToBeReturned= cashGivenValue-billAmmountValue;
+    // console.log(ammountToBeReturned);
+    // if(ammountToBeReturned>0){
+    //     calculateChange(ammountToBeReturned);
+    // }
+    // else{
+    //     //Bill is more than cash value. Invalid
+    //     errorMessage.style.display= "block";
+    //     showErrorMessage("Bill is more than the cash you have given. No Bargaining here!");
+    // }
 
 
 
